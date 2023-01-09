a1f034ad1cdaf03de8c1f94992c47e8226430ee1
Kirill Gontsov <mail@kgontsov.ru>
2023-01-09 22:37:38 +0300

    Добавил кнопку и счетчик

--- sprint 3 code rew/ViewController.swift
+++ sprint 3 code rew/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scoreInc: UIButton!
+    @IBOutlet weak var scoreCount: UILabel!
+    
+    private var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        
+        scoreInc.backgroundColor = UIColor.systemMint
     }
 
+    @IBAction func tapToInc(_ sender: Any) {
+        score += 1
+        scoreCount.text = "Значение счетчика: \(score)"
+    }
+    
 
 }
 
